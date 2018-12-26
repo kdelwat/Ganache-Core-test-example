ded21e0a9abcd35e246a635df86c40a8af5c53fe
Cadel Watson <cadel@cadelwatson.com>
2018-12-27 10:52:31 +1100

    Failing test example

---
+++ ganache.test.ts
@@ -0,0 +1,8 @@
+// @ts-ignore
+import Ganache = require("ganache-core");
+
+describe("a set of tests using Ganache", () => {
+    it("should complete successfully", async () => {
+        Ganache.provider()
+    });
+});
